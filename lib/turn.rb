f8bbf21868dca91594c6a03cdf9e2c1f8afdc0c9
David Reed <djreed20@gmail.com>
2021-02-15 23:50:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,15 +13,13 @@ def input_to_index(user_input)
 end
 
 def position_taken?(board, index)
-  if board[index] == " "
-    false
-  elsif board[index] == ""
-    false
-  elsif board[index] == nil
+  if board[index] == " " || board[index] == "" || board[index] == nil
     false
   else
     true
   end
+end
+
 
 
 def valid_move?(board, index)
