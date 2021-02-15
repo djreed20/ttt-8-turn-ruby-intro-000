e99fb02cb7e41e2561f7a32d5396041b12450b04
David Reed <djreed20@gmail.com>
2021-02-15 23:51:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,9 +21,11 @@ def position_taken?(board, index)
 end
 
 
-
 def valid_move?(board, index)
-  if index.between?(1,9)
-    if !position_taken?(board, index)
+  if !position_taken?(board, index) && (index).between?(0, 8)
       true
+    else
+      false
     end
+  end
+
